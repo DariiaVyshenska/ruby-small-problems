786b64c134b7dbcdaf74ebbd979c9e0a97b9be24
DariiaVyshenska <vyshenskaia@gmail.com>
2020-10-26 23:21:57 +0000

    all exercise solutions for chapter Methods

---
+++ Methods/mutate.rb
@@ -0,0 +1,18 @@
+a = [1, 2, 3]
+
+# Example of a method definition that modifies its argument permanently
+def mutate(array)
+  array.pop
+end
+
+p "Before mutate method: #{a}"
+p mutate(a)
+p "After mutate method: #{a}"
+
+# def no_mutate(a)
+#   a.last
+# end
+
+# p "Before no_mutate method: #{a}"
+# no_mutate(a)
+# p "After no_mutate method: #{a}"
