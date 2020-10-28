e1d04a1d1060db98bf9747de8c1a70dce444b553
DariiaVyshenska <vyshenskaia@gmail.com>
2020-10-28 22:41:26 +0000

    All solutions for exercises from 'Arrays' chapter

---
+++ Arrays/1_arrays.rb
@@ -0,0 +1,8 @@
+# Write a program that checks to see if the number appears in the array.
+
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+if arr.include?(number)
+  puts "Yes, your number is in the array!"
+end
---
+++ Arrays/7_arrays.rb
@@ -0,0 +1,3 @@
+names = ['Bobik', 'Sharik', 'Vasya', 'Ryzhik']
+
+names.each_with_index {|val, idx| puts "Index for #{val} is #{idx}"}
